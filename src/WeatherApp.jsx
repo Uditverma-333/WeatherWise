629dad3aa9f66017031bb1d6ed2d9cf0621152f5
Udit Verma <uditverma150@gmail.com>
2024-05-19 10:37:50 +0530

    Deploying new weather application

---
+++ src/WeatherApp.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import InfoBox from "./InfoBox";
+import SearchBox from "./SearchBox";
+
+export default function WeatherApp() {
+    const [weatherInfo, setWeatherInfo] = useState({
+        city: "Mumbai",
+        feelsLike: 24.84,
+        temp: 25.05,
+        tempMin: 25.05,
+        tempMax: 25.05,
+        humidity: 47,
+        weather: "haze",
+    });
+
+    let updateInfo = (newInfo) => {
+        setWeatherInfo(newInfo);
+    };
+
+    return (
+        <div style={{textAlign: "center"}}>
+            <h2>WeatherWise</h2>
+            <SearchBox updateInfo={updateInfo}/>
+            <InfoBox info={weatherInfo}/>
+        </div>
+    );
+}
